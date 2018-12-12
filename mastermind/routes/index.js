b186b1ef3661d04b47d433f39d75d5667aafec5c
unknown <jelleromer@live.nl>
2018-12-12 11:21:09 +0100

    Add assignment 2.3.1 and 2.3.2

--- mastermind/routes/index.js
+++ mastermind/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// none of this code works.
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+/* GET home page. */
+// router.get("/", function (req, res) {
+//   res.sendFile("splash.html", {root: "./public"});
+// });
+// app.get("/", (req, res) => {
+//   res.sendFile("splash.html", {root: "./public"});
+// });
 
 module.exports = router;
